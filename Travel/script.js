3086c928fe72a70388954cb1ea6a0c82b72892a6
CSteigueber <c.steigueber@gmail.com>
2019-11-21 16:48:08 +0100

    feature added search countries with same safety

--- Travel/script.js
+++ Travel/script.js
@@ -9,13 +9,29 @@ const getIso = (str, countries, arr)=>{
     return str;
 }
 
-(async function (){
+const printArr =(str,arr)=>{
+    let score=arr[str].advisory.score;
+    let div=document.getElementById("output");
+    let p=document.createElement("p");
+    switch (true){
+        case score>4.5: p.setAttribute("class", "bg-danger");  break;
+        case score>3.5: p.setAttribute("class", "bg-warning"); break;
+        case score>2.5: p.setAttribute("class", "bg-info");    break;
+        case score>0:   p.setAttribute("class", "bg-success"); break;            
+    }
+    let text=`The safety score in ${arr[str].name} is ${arr[str].advisory.score}`;
+    let node= document.createTextNode(text);
+    p.appendChild(node);
+    div.appendChild(p);
+}
+//-------------------------main function------------------------------------------
+(async function main(){
     var abc="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(" ");
     var url="https://www.travel-advisory.info/api";
     var api= await fetch(url);
     var arr= await api.json();
     arr=arr.data;
-
+//------------------------------------------get a list of available country codes
     let countries=[];
     for (let i=0; i<abc.length;i++){
         for (let j=0;j<abc.length;j++){
@@ -24,53 +40,38 @@ const getIso = (str, countries, arr)=>{
             }
         }
     }
-    console.log(countries);
-
-    console.log(arr);
 
     //Get a countries safety:
     document.getElementById("run").addEventListener("click",function (){
         let str=document.getElementById("input").value;
         let safety=999;
-    // If a country name is entered its iso code must be found:       
-        str=getIso(str, countries, arr);
+           
+        str=getIso(str, countries, arr);   // If a country name is entered its iso code must be found
         if (!arr[str]){
             console.log(str+" not found");
         }
         else {
-            let score=arr[str].advisory.score;
-            let div=document.getElementById("output");
-            let p=document.createElement("p");
-            switch (true){
-                case score>4.5: p.setAttribute("class", "bg-danger");  break;
-                case score>3.5: p.setAttribute("class", "bg-warning"); break;
-                case score>2.5: p.setAttribute("class", "bg-info");    break;
-                case score>0:   p.setAttribute("class", "bg-success"); break;            
-            }
-
-            let node= document.createTextNode(" The safety score in "+arr[str].name+" is "+arr[str].advisory.score);
-            p.appendChild(node);
-            div.appendChild(p);
+            printArr(str,arr);
         }
     })
 
     //Find countries that are of same safety:
     document.getElementById("same").addEventListener("click", function same(){
-        let str=document.getElementById("input").value;
+        let str=getIso(document.getElementById("input").value,countries,arr);
+        let score=arr[str].advisory.score;
         let res=[];
-        for (i=0;i<countries;i++){
-            if (arr[countries[i]].advisory.score=="later"){
-                console.log("later");
+        for (i=0;i<countries.length;i++){                   //Go through the coutries to look for same safety
+            if (arr[countries[i]].advisory.score==score){
+                res.push(arr[countries[i]].name);
             }
         }
+        for (let i=0; i<res.length;i++){                    //Print all res 
+            let iso=getIso(res[i],countries,arr);
+            printArr(iso,arr);
+        }
     })
-
-    // Clear output:
-    document.getElementById("clear").addEventListener("click", function clear (){
-        document.getElementById("output").innerHTML="";
+    
+    document.getElementById("clear").addEventListener("click", function clear (){ 
+        document.getElementById("output").innerHTML="";                             // Clear output:
     })
-
-
-
-    console.log("end of script!");
 })();
