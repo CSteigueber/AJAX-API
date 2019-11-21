fbc2423aba7c19e08d959c9e6791a03f884e7c8b
CSteigueber <c.steigueber@gmail.com>
2019-11-21 12:31:49 +0100

    lunch break

--- Travel/script.js
+++ Travel/script.js
@@ -1,16 +1,25 @@
-var mapsKey="";
-
-  
-
 (async function (){
+var abc="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(" ");
+console.log(abc);
 var url="https://www.travel-advisory.info/api";
 var api= await fetch(url);
 var arr= await api.json();
 arr=arr.data;
+
 document.getElementById("run").addEventListener("click",function (){
-    console.log(arr);
     let str=document.getElementById("input").value;
     let safety=999;
+// If a country name is entered its iso code must be found:
+    if (str.length>2){
+        for (let i=0; i<abc.length;i++){
+            for (let j=0;i<abc.length; j++){
+                if (arr[abc[i]+abc[j]]!=undefined){
+                    console.log(abc[i]+abc[j]);
+                }
+            } 
+        }
+        console.log("finished")
+    }
 
     if (!arr[str]){
         console.log(str+" not found");
