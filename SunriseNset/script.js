cf61cb471bdcf73018e4d80ab0a11bdc14cfbecc
CSteigueber <c.steigueber@gmail.com>
2019-11-26 10:21:15 +0100

    added dates to choose

--- SunriseNset/script.js
+++ SunriseNset/script.js
@@ -2,7 +2,6 @@ var lat=0;
 var lng=0;
 function getPosition(){
      if(navigator.geolocation){
-         console.log("Howdie partner!");
         navigator.geolocation.getCurrentPosition(onPositionUpdate);
         
     }
@@ -20,8 +19,8 @@ function setNodes(id,value){
     p.appendChild(node)
     document.getElementById(id).appendChild(p);
 }
-function setResults(arr, adress){
-    setNodes("location", adress);
+function setResults(arr, adress,date){
+    setNodes("location", `${adress} ${date}`);
     setNodes("a-twil-begin",arr.astronomical_twilight_begin)
     setNodes("n-twil-begin",arr.nautical_twilight_begin)
     setNodes("c-twil-begin",arr.civil_twilight_begin)
@@ -37,8 +36,8 @@ function onPositionUpdate(position)
     lat = position.coords.latitude;
     lng = position.coords.longitude;
 }
-async function getData (lat, lng){
-        let url=`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`;
+async function getData (lat, lng,date){
+        let url=`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=${date}`;
         let api= await fetch(url);
         let arr= await api.json();
         arr=arr.results;
@@ -48,29 +47,30 @@ async function getAdress(lat, lng){
        url=`https://geocode.xyz/${lat},${lng}?json=1`;
        api= await fetch(url);
        res= await api.json();
-       let location=`${res.city}, ${res.country}, timezone: ${res.timezone}`;
-       console.log(res);
+       let location=`${res.city}, ${res.country}`;
        return(location);
 }
 
 (function (){
     getPosition();
     document.getElementById("local").addEventListener("click",async function localData(){
-        let arr=await getData(lat,lng);
+        let date=`today`;
+        let arr=await getData(lat,lng,date);
         let location=await getAdress(lat,lng);
-        setResults(arr,location);
+        setResults(arr,location,date);
     })
    
     document.getElementById("search").addEventListener("click", async function remoteData(){
        let adress=document.getElementById("input").value;
+       let date=document.getElementById("date").value;
        let url=`https://geocode.xyz/${adress}?json=1`;
        let api=await fetch(url);
        let res= await api.json();
        let lat= res.latt;
        let lng= res.longt;
        let location=await getAdress(lat,lng);
-       let arr=await getData(lat,lng);
-       setResults(arr,location);
+       let arr=await getData(lat,lng,date);
+       setResults(arr,location,`at ${date}`);
    })
 
 })();
